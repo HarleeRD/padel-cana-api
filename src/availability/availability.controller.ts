ac7f9b5da3b92086c1ac93a17015ea4de317db60
harleeonline24-cyber <harleeonline24@gmail.com>
2026-02-06 21:24:36 +0000

    Tag availability endpoints in Swagger

--- src/availability/availability.controller.ts
+++ src/availability/availability.controller.ts
@@ -1,6 +1,8 @@
 import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AvailabilityService } from './availability.service';
 
+@ApiTags('Availability')
 @Controller('availability')
 export class AvailabilityController {
   constructor(private readonly availabilityService: AvailabilityService) {}
